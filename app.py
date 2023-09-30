517d7aff56e1ffdb19f3878d5117f15fd62f753d
shinyorke <spirits.is.my.rader@gmail.com>
2023-09-30 17:35:57 +0900

    sample

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import random
+import time
+
+st.title("阪神タイガース優勝をドヤるChat App")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("阪神タイガースについて何でも聞いてみて"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "そら（誰よりもお散歩しているから）、そうよ。",
+                "そら（他のチームの散歩を止めてるから）、そうよ。",
+                "知らんがな",
+                "なんでや！！！阪神！！！関係あるやろ！！！！",
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
