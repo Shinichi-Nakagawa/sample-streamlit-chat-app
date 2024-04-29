451662df6798bee7e9bb2d5f27df1c11555b7144
shinichi.nakagawa@shinyorke <spirits.is.my.rader@gmail.com>
2024-04-29 15:57:06 +0900

    依存ライブラリ更新

--- graph.py
+++ graph.py
@@ -11,6 +11,19 @@ HEIGHT = 700
 def bar(df: pd.DataFrame, x: List[str], y: str, title: str, x_title: str, y_title: str,
         x_dtick: int, x_range: Tuple[int], autosize: bool = True) -> go.Figure:
     fig = px.bar(df, x=x, y=y, barmode='group', )
+    """
+    Bar chart
+    :param df: DataFrame
+    :param x: X column
+    :param y: Y column
+    :param title: Graph title
+    :param x_title: x-axis title
+    :param y_title: y-axis title
+    :param x_dtick: x-axis tick interval
+    :param x_range: x-axis range
+    :param autosize: Auto size flag
+    :return: Figure object
+    """
     if autosize:
         # 画面に合わせるパターン
         fig.update_layout(
